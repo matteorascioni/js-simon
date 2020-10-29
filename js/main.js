f2d1d2409d44f97b680a190a4dcf171430a37d8d
Matteo <Rascioni>
2020-10-29 16:30:36 +0100

    alert 50%

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
-
     // Ref
-    var fiveAlertNumbers = randomNumbers(1, 50);
+    var randomNumbersArray = [];
+    var size = 5
+
+    while (randomNumbersArray.length < size) {
+        var randomNumbers = getRandomNumbers(1, 50);
+
+        if(! randomNumbersArray.includes(randomNumbers)) {
+            randomNumbersArray.push(randomNumbers);
+        }
+        alert(randomNumbersArray);
+    }   
+
     
-
-    // Ciclo alert con numeri random
-    for(var i = 0; i < 5; i++) {
-        alert(fiveAlertNumbers)
-    }
-
-    // Ciclo richieste utente
-    
-
 
 }); //  End of Jquery document
 
 // Funzione generazione numeri random
-function randomNumbers(min, max) {
+function getRandomNumbers(min, max) {
     return Math.floor(Math.random() * (max - min +1 ) + min );
 }   
