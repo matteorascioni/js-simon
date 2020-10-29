7c40758960e6d9abaad403132a8dbcdaaec1721e
Matteo <Rascioni>
2020-10-29 16:13:55 +0100

    start

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    console.log('hello');
-});
+
+    // Ref
+    var fiveAlertNumbers = randomNumbers(1, 50);
+    
+
+    // Ciclo alert con numeri random
+    for(var i = 0; i < 5; i++) {
+        alert(fiveAlertNumbers)
+    }
+
+    // Ciclo richieste utente
+    
+
+
+}); //  End of Jquery document
+
+// Funzione generazione numeri random
+function randomNumbers(min, max) {
+    return Math.floor(Math.random() * (max - min +1 ) + min );
+}   
